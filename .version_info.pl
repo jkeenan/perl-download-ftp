cf03e705e09a5e44bd0e400fe5e1787f301672a1
James E Keenan <jkeenan@cpan.org>
2017-12-04 17:42:01 -0500

    CPAN version 0.03

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
     version => { old => '0.02', new => '0.03' },
-    date    => { old => 'December 01 2017', new => 'December 03 2017' },
+    date    => { old => 'December 03 2017', new => 'December 04 2017' },
     copyright => { old => '2017', new => '2017' },
 );
 
